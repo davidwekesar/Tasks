097f973bbff39b5259a6d65d79d54be1f3e6b217
davidwekesar <davidwekesar@gmail.com>
2021-01-26 16:49:17 +0000

    Navigation: HomeFragment to TasksFragment

--- app/src/main/java/com/awalideck/tasks/main/MainActivity.kt
+++ app/src/main/java/com/awalideck/tasks/main/MainActivity.kt
@@ -2,11 +2,16 @@ package com.awalideck.tasks.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
 import com.awalideck.tasks.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
     }
 }
--- app/src/main/java/com/awalideck/tasks/tasks/TasksFragment.kt
+++ app/src/main/java/com/awalideck/tasks/tasks/TasksFragment.kt
@@ -1,7 +1,33 @@
 package com.awalideck.tasks.tasks
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.awalideck.tasks.R
+import com.awalideck.tasks.databinding.FragmentTasksBinding
 
-class TasksFragment : Fragment(R.layout.fragment_tasks) {
+class TasksFragment : Fragment() {
+
+    private var _binding: FragmentTasksBinding? = null
+    private val binding get() =  _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTasksBinding.inflate(inflater, container, false)
+
+        val quadNum = TasksFragmentArgs.fromBundle(requireArguments()).quadNum
+        val textView = binding.quadNumber
+        textView.text = "Quadrant number: $quadNum"
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
